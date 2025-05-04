df141a28dc90dbbcd09c994e982a9c5f50585e14
JulianDr14 <julianda.rodriguez@campusucc.edu.co>
2025-05-03 23:31:26 -0500

    feat: Implementación de la página de inicio y mejora de la funcionalidad de inicio de sesión
    Este commit introduce la página de inicio y mejora la experiencia de inicio de sesión:
    
    - Página de inicio:
        - Se agregó la clase `HomePage` como la página principal de la aplicación.
        - Muestra el texto "Welcome to the Home Page" en el centro de la pantalla.
    - Enrutamiento:
        - Se añadió la ruta '/home' en `AppRouter` para redirigir a `HomePage`.
    - Página de inicio de sesión:
        - Se actualizó la página de inicio de sesión para navegar a '/home' tras un inicio de sesión exitoso.
        - Se agregó la visualización de errores de inicio de sesión usando `SnackbarService`.
        - Se eliminó la pantalla `HomePage` que solo estaba de prueba.

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:intentary_pro/features/auth/presentation/pages/login_page.dart';
 import 'package:intentary_pro/features/error_screen/screens/error_screen.dart';
+import 'package:intentary_pro/features/home/presentation/home_page.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intentary_pro/core/services/snackbar_service.dart';
 
 import 'package:intentary_pro/features/auth/di/auth_providers.dart';
 import 'package:intentary_pro/features/auth/presentation/viewmodels/auth_viewmodel.dart';
@@ -27,8 +28,18 @@ class _LoginPageState extends ConsumerState<LoginPage> {
   @override
   Widget build(BuildContext context) {
     ref.listen<AuthState>(authViewModelProvider, (prev, next) {
-      if (prev?.loading == true && next.loading == false && next.error == null) {
+      if (prev?.loading == true &&
+          next.loading == false &&
+          next.error == null) {
         context.go('/home');
+      }
+
+      if (next.error != null) {
+        SnackbarService.show(
+          context,
+          message: next.error!,
+          type: SnackbarType.error,
+        );
       }
     });
 
@@ -42,69 +53,86 @@ class _LoginPageState extends ConsumerState<LoginPage> {
               padding: const EdgeInsets.all(16),
               child: Form(
                 key: _formKey,
-                child: Column(mainAxisSize: MainAxisSize.min, children: [
-                  Text('Bienvenido',
-                      style: Theme.of(context).textTheme.titleLarge),
-                  Text(
-                    'Inicia sesión para continuar',
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    controller: _userCtrl,
-                    decoration: const InputDecoration(labelText: 'Usuario'),
-                    validator: (v) =>
-                    v == null || v.isEmpty ? 'Ingresa tu usuario' : null,
-                  ),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    controller: _passCtrl,
-                    decoration:
-                    const InputDecoration(labelText: 'Contraseña'),
-                    obscureText: true,
-                    validator: (v) => v == null || v.isEmpty
-                        ? 'Ingresa tu contraseña'
-                        : null,
-                  ),
-                  const SizedBox(height: 16),
-                  Consumer(builder: (context, ref, _) {
-                    final AuthState authState = ref.watch(authViewModelProvider);
-                    final AuthViewModel authNotifier = ref.read(authViewModelProvider.notifier);
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Bienvenido',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Text(
+                      'Inicia sesión para continuar',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    const SizedBox(height: 8),
+                    TextFormField(
+                      controller: _userCtrl,
+                      decoration: const InputDecoration(labelText: 'Usuario'),
+                      validator:
+                          (v) =>
+                              v == null || v.isEmpty
+                                  ? 'Ingresa tu usuario'
+                                  : null,
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: _passCtrl,
+                      decoration: const InputDecoration(
+                        labelText: 'Contraseña',
+                      ),
+                      obscureText: true,
+                      validator:
+                          (v) =>
+                              v == null || v.isEmpty
+                                  ? 'Ingresa tu contraseña'
+                                  : null,
+                    ),
+                    const SizedBox(height: 16),
+                    Consumer(
+                      builder: (context, ref, _) {
+                        final AuthState authState = ref.watch(
+                          authViewModelProvider,
+                        );
+                        final AuthViewModel authNotifier = ref.read(
+                          authViewModelProvider.notifier,
+                        );
 
-                    return Column(
-                      children: [
-                        if (authState.error != null)
-                          Text(authState.error!, style: const TextStyle(color: Colors.red)),
-                        const SizedBox(height: 8),
-                        SizedBox(
-                          width: double.infinity,
-                          child: FilledButton(
-                            onPressed: authState.loading
-                                ? null
-                                : () {
-                              if (_formKey.currentState!.validate()) {
-                                authNotifier.login(
-                                  _userCtrl.text.trim(),
-                                  _passCtrl.text.trim(),
-                                );
-                              }
-                            },
-                            child: authState.loading
-                                ? const SizedBox(
-                              width: 24,
-                              height: 24,
-                              child: CircularProgressIndicator(
-                                strokeWidth: 2,
-                                color: Colors.white,
+                        return Column(
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              child: FilledButton(
+                                onPressed:
+                                    authState.loading
+                                        ? null
+                                        : () {
+                                          if (_formKey.currentState!
+                                              .validate()) {
+                                            authNotifier.login(
+                                              _userCtrl.text.trim(),
+                                              _passCtrl.text.trim(),
+                                            );
+                                          }
+                                        },
+                                child:
+                                    authState.loading
+                                        ? const SizedBox(
+                                          width: 24,
+                                          height: 24,
+                                          child: CircularProgressIndicator(
+                                            strokeWidth: 2,
+                                            color: Colors.white,
+                                          ),
+                                        )
+                                        : const Text('Login'),
                               ),
-                            )
-                                : const Text('Login'),
-                          ),
-                        ),
-                      ],
-                    );
-                  })
-                ]),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -112,14 +140,4 @@ class _LoginPageState extends ConsumerState<LoginPage> {
       ),
     );
   }
-}
-
-// Pantalla debugin para verificar que el login fue exitoso
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-  @override
-  Widget build(BuildContext c) => Scaffold(
-    appBar: AppBar(title: const Text('Home')),
-    body: const Center(child: Text('¡Has iniciado sesión!')),
-  );
 }
